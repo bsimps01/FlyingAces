a54e062a56e199ca10b1513a8442719661e9cfd5
Ben Simpson <b.albertsimpson@gmail.com>
2020-01-08 19:00:39 -0800

    Added moving background and enemy planes

--- FlyingAces/FlyingAces/GameScene.swift
+++ FlyingAces/FlyingAces/GameScene.swift
@@ -9,102 +9,104 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var Clouds = SKSpriteNode()
+    var player:SKSpriteNode!
+    var scoreLabel:SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var gameTimer: Timer!
+    var enemyPlanes = ["enemy1", "enemy2", "enemy3"]
+    let enemyCategory: UInt32 = 0x1 << 1
+    let bulletCategory:UInt32 = 0x1 << 0
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView){
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        player = SKSpriteNode(imageNamed: "player")
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        self.addChild(player)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        self.anchorPoint = CGPoint(x: 0.5, y: 0)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 120, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "Arial"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.yellow
+        score = 0
+        
+        self.addChild(scoreLabel)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addPlane), userInfo:nil, repeats: true)
+        
+        createClouds()
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    @objc func addPlane() {
+        enemyPlanes = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: enemyPlanes) as! [String]
+        
+        let enemy = SKSpriteNode(imageNamed: enemyPlanes[0])
+        
+        let randomEnemyPosition = GKRandomDistribution(lowestValue: 0, highestValue: 414)
+        let position = CGFloat(randomEnemyPosition.nextInt())
+        
+        enemy.position = CGPoint(x: position, y: self.frame.size.height + enemy.size.height)
+        
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.isDynamic = true
+        
+        enemy.physicsBody?.categoryBitMask = enemyCategory
+        enemy.physicsBody?.contactTestBitMask = bulletCategory
+        enemy.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(enemy)
+        
+        let animationDuration: TimeInterval = 6
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -enemy.size.height), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        
+        enemy.run(SKAction.sequence(actionArray))
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        moveClouds()
+}
+
+    func createClouds() {
+        for i in 0...3 {
+            let Clouds = SKSpriteNode(imageNamed: "Clouds")
+            Clouds.name = "Clouds"
+            Clouds.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!)
+            Clouds.anchorPoint = CGPoint(x: 0.0, y: 0)
+            Clouds.position = CGPoint(x: -(self.frame.size.width / 2), y: CGFloat(i) * Clouds.size.height)
+            Clouds.zPosition = -1
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+            self.addChild(Clouds)
     }
+}
+
+    func moveClouds() {
+    
+        self.enumerateChildNodes(withName: "Clouds", using: ({
+            (node, error) in
+        
+            node.position.y -= 6
+        
+            if node.position.y < -((self.scene?.size.height)!) {
+            
+                node.position.y += (self.scene?.size.height)! * 3
+        }
+    }))
+}
 }
